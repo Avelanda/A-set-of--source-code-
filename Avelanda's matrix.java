a4e40fd0dd6fc0c18d49fd1434c409577ed9a35b
Avelanda <gorthell@gmail.com>
2021-10-15 11:55:09 +0200

    Add files via upload
    A symmetrically composed set: yielding a sequence of numbers which increases and decreases, vertically, horizontally, and diagonally; or remaining constant- in reverse or forward order.

---
+++ Avelanda's matrix.java
@@ -0,0 +1,131 @@
+import java.util.*;
+
+// Compiler version JDK 11.0.2
+
+/* Copyright © 2021
+   Avelanda
+   All rights reserved */
+     
+/* Algorithmic computation of Avelanda's matrix */
+
+class Matrix
+{
+ public static void main(String args[])
+  { 
+   for (int i = 0; i < 10; i++)
+      System.out.print(i + " ");
+   for (int i = 0; i == 0; i++)
+      System.out.print(i + " ");
+   for (int i = 9; i >= 0; i--)
+      System.out.print(i + " ");
+   System.out.println();
+  {
+   for (int i = 1; i < 10; i++)
+      System.out.print(i + " ");
+   for (int i = 0; i == 0; i++)
+      System.out.print(i + " ");
+   for (int i = 9; i >= 0; i--)
+      System.out.print(i + " ");
+   for (int i = 9; i > 8; i--)
+      System.out.print(i + " ");
+   System.out.println();
+  {
+   for (int i = 2; i < 10; i++) 
+      System.out.print(i + " ");
+   for (int i = 0; i == 0; i++)
+      System.out.print(i + " ");
+   for (int i = 9; i >= 0; i--)
+      System.out.print(i + " ");
+   for (int i = 9; i > 7; i--)
+      System.out.print(i + " ");
+   System.out.println();
+  {
+   for (int i = 3; i < 10; i++)
+      System.out.print(i + " ");
+   for (int i = 0; i == 0; i++)
+      System.out.print(i + " ");
+   for (int i = 9; i >= 0; i--)
+      System.out.print(i + " ");
+   for (int i = 9; i > 6; i--)
+      System.out.print(i + " ");
+   System.out.println();
+  {
+   for (int i = 4; i < 10; i++)
+      System.out.print(i + " ");
+   for (int i = 0; i == 0; i++)
+      System.out.print(i + " ");
+   for (int i = 9; i >= 0; i--)
+      System.out.print(i + " ");
+   for (int i = 9; i > 5; i--)
+      System.out.print(i + " ");
+   System.out.println();
+  {
+   for (int i = 5; i < 10; i++)
+      System.out.print(i + " ");
+   for (int i = 0; i == 0; i++)
+      System.out.print(i + " ");
+   for (int i = 9; i >= 0; i--)
+      System.out.print(i + " ");
+   for (int i = 9; i > 4; i--)
+      System.out.print(i + " ");
+   System.out.println();
+  {
+   for (int i = 6; i < 10; i++)
+      System.out.print(i + " ");
+   for (int i = 0; i == 0; i++)
+      System.out.print(i + " ");
+   for (int i = 9; i >= 0; i--)
+      System.out.print(i + " ");
+   for (int i = 9; i > 3; i--)
+      System.out.print(i + " ");
+   System.out.println();
+  {
+   for (int i = 7; i < 10; i++)
+      System.out.print(i + " ");
+   for (int i = 0; i == 0; i++)
+      System.out.print(i + " ");
+   for (int i = 9; i >= 0; i--)
+      System.out.print(i + " ");
+   for (int i = 9; i > 2; i--)
+      System.out.print(i + " ");
+   System.out.println();
+  {
+   for (int i = 8; i < 10; i++)
+      System.out.print(i + " ");
+   for (int i = 0; i == 0; i++)
+      System.out.print(i + " ");
+   for (int i = 9; i >= 0; i--)
+      System.out.print(i + " ");
+   for (int i = 9; i > 1; i--)
+      System.out.print(i + " ");
+   System.out.println();
+  {
+   for (int i = 9; i < 10; i++)
+      System.out.print(i + " ");
+   for (int i = 0; i == 0; i++)
+      System.out.print(i + " ");
+   for (int i = 9; i >= 0; i--)
+      System.out.print(i + " ");
+   for (int i = 9; i > 0; i--)
+      System.out.print(i + " ");
+   System.out.println();
+  {
+   for (int i = 0; i == 0; i++)
+      System.out.print(i + " ");
+   for (int i = 9; i >= 0; i--)
+      System.out.print(i + " ");
+   for (int i = 9; i >= 0; i--)
+      System.out.print(i + " ");
+   System.out.println();
+  }
+ }
+ }
+ }
+ }
+ }
+ }
+ }
+ }
+ }
+ }
+ }
