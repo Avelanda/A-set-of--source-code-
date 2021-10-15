cf002c7ece2ead8869d1e693124df6e591e8d95d
Avelanda <gorthell@gmail.com>
2021-10-15 11:43:26 +0200

    Add files via upload
    Output compilation through nesting of various methods.

---
+++ Hello World.java
@@ -0,0 +1,150 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+ /* Copyright © 2021
+    Avelanda
+    All rights reserved
+ */
+ 
+ /* Execution of a Hello World program */
+class Hello_World
+ {
+  public static void main(String args[])
+  { 
+   System.out.print("Hello ");
+   System.out.println("World");
+  }
+ } 
+ 
+ /* Utilising a substring method for executing a hello world program */
+public class hello_world
+ {
+  public static void main(String arg[])
+  {
+   String greeting = "Hello World";
+   greeting.substring(0,1,2,3,4,5,6,7,8,9);
+  }
+ }
+ 
+ /* Utility of string comparison to execute a hello world program */  
+public class HELLO_WORLD
+ {
+  public static void main(String[] args)
+  {
+   String Hello_World = "Hello World";
+   String hello_world = "Hello World";
+   if (Hello_World.equals(hello.world))
+  {
+   System.out.println("Hello World");
+  }
+ }
+ 
+ /* Execution of a hello world program using dot notation */
+public class HellO_WorlD
+ {
+  public static void main(String[] args)
+  {
+   String x = "Hello";
+   String y = "World";
+   String x = blank.x; 
+   String y = blank.y;
+   System.out.println("blank.x" + "blanky");
+  }
+ }
+ 
+ /* Utilising indexes to execute a hello world program */
+class HELLO_world
+ {
+  public static void main(String[] args)
+  {
+   int [] counts = new int [10];
+   int i = 0;
+  {
+   System.out.println(counts[i]);
+   i++;
+  }
+ }
+
+ /* Executing a hello world program through an integer conversion into a string */
+public class hello_WORLD
+ {
+  public static void main(String[] args)
+   {
+    String str = "Hello World";
+    int num = Integer.parseInt(str);
+   {
+    System.out.println(str);
+   }
+  }
+  
+ /* Executing a hello World program */
+class heLLo_woRLd
+ {
+  public static void main(String [] args)
+   {
+    int name = "Hello World";
+    name.stream()
+     .map(name -> name.toString())
+     .forEach(name -> System.out.print(name + " "));
+    System.out.println();
+   }
+  }
+  
+ /* Utility of characters to execute a hello world program */
+class HeLLO_WoRLD
+ {
+  public static void main(String [] args)
+   {
+    String greeting = "Hello World";
+    char letter = greeting.charAt(0 - 10);
+    if (letter == greeting)
+   {
+    System.out.println(greeting.charAt(0 - 10));
+   }
+  }
+
+ /* Nesting and comparison of classes */
+class greeting
+ {
+  public static void main(String [] args)
+   {
+    if (Hello_World == hello_world)
+   {
+    System.out.println("Hello World");
+   } else {
+    if (hello_world == HELLO_WORLD)
+   {
+    System.out.println("Hello World");
+   } else {
+    if (HELLO_WORLD == HellO_WorlD)
+   {
+    System.out.println("Hello World");
+   } else {
+    if (HellO_WorlD == HELLO_world)
+   {
+    System.out.println("Hello World");
+   } else {
+    if (HELLO_world == hello_WORLD)
+   {
+    System.out.println("Hello World");
+   } else {
+    if (hello_WORLD == heLLo_woRLd)
+   {
+    System.out.println("Hello World");
+   } else {
+    if (heLLo_woRLd == HeLLO_WoRLD)
+   {
+    System.out.println("Hello World");
+  }
+ }
+}
+}
+}
+}
+}
+}
+}
+}
+} 
+}
+}    
