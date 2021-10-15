b0c24d56696996c63717de4c4a12732bce2102a1
Avelanda <gorthell@gmail.com>
2021-10-15 12:12:07 +0200

    Add files via upload
    Assigning variables, and utilising a set of different methods to out the program on specified run-time.

---
+++ Hello World.py
@@ -0,0 +1,78 @@
+#python 3.7.1
+
+# Copyright © 2021
+# Avelanda
+# All rights reserved
+
+print ("Hello World")
+
+   # Repeatition of an operation without altering a return value
+print ("Hello World" * 0 )
+
+   # Concatenation of strings
+str1  = "Hello"
+str2  = "World"
+str3  = str1 + str2 
+
+lines = []
+while True:
+   str: input()
+   if str:
+          def printValue():
+             print(str(n))
+             printValue()
+    
+   # Processing runtime 
+import time
+import random
+class Execution_Time:
+   def__init__(self)
+   self.start_time = time.time()
+Timer = Execution_time()
+s = [random.randint(1,0)
+    for num in 
+    range (1,1000000)
+    if num% 3 == 0]
+print('finished in {} seconds.'.format(timer.duration()))
+
+   # A class creating callable instances
+if n == "Hello":
+   m == "World"
+
+class Adder(object):
+    def_init_(self, n);
+    self.n = n
+    def_call_(self, m)
+print("n + m")
+
+   # A functional closure 
+str1 = "n"
+str2 = "m"
+str3 == n + m
+
+def make_adder(n):
+     def adder(m):
+      return n + m
+     return adder
+    
+   # Calling an adder function
+x == "n"
+y == "m"
+
+def adder(x, y):
+    return x + y
+
+  # Defining and calling a function
+a = "x"
+b = "y"
+
+def combine(a,b):
+    return 0, a[1] + b[1]
+    
+  # Calling a function through value assignment
+name = "value"
+value == "Hello World"
+
+def print_assign(name, value):
+    return name + '=' + str(value)
+       
