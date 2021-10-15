1128c0adb3aabc05f3d816a584d1612a691bf162
Avelanda <gorthell@gmail.com>
2021-10-15 12:04:05 +0200

    Add files via upload
    Definition of, and assignment of variables; and representation of strings with respect to the UTF-8 code.

---
+++ Recursion.swift
@@ -0,0 +1,54 @@
+//Swift 5.0
+
+/* Copyright © 2021
+   Avelanda
+   All rights reserved
+*/
+  
+/* String representation in Unicode. */
+print(" ")
+var a = "I'm"
+for codeUnit in a.utf8
+{
+ var b = "just"
+ for codeUnit in b.utf8
+{
+ var c = "a"
+ for codeUnit in c.utf8
+{
+ var d = "fool"
+ for codeUnit in d.utf8
+{
+ var e = "that"
+ for codeUnit in e.utf8
+{
+ var f = "never"
+ for codeUnit in f.utf8
+{
+ var g = "believed"
+ for codeUnit in g.utf8
+{
+ var h = "it"
+ for codeUnit in h.utf8
+{
+ var i = "was"
+ for codeUnit in i.utf8
+{
+ var j = "stupid."
+ for codeUnit in j.utf8
+{
+/* Concatenation of strings */
+ var x = a + " " + b + " " + c + " " +
+              d + " " + e + " " + f + " " + 
+              g + " " + h + " " + i + " " + j
+ print(x)
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
