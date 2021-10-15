94cd4b9f970b05cd3a877baabcde2fc9f21ada09
Avelanda <gorthell@gmail.com>
2021-10-15 12:16:22 +0200

    Add files via upload
    Approximating π using the exponential equation (where is a constant); such that as x increases: the value of y approaches pi- as it corrects to decimal places.

---
+++ Approximating pi.py
@@ -0,0 +1,44 @@
+#python 3.7.1
+
+# Avelanda
+# Copyright © 2021
+# All rights reserved
+
+import math
+
+# Approximation of pi
+x = 9.869604401089358618833174
+35535099146914691464501211
+98356867806617978125769073
+82899412141254156600984407
+a = 0.5
+y = x ** a
+print("f(x) = x ** a")
+def make_adder(x):
+  print(x ** a)
+{
+ pow(x, a):
+  print(x ** a)
+}
+
+# The value of pi
+x = 2
+a = math.pi / 4
+y = a * (x ** 2)
+print(" ")
+print("π =")
+print(math.pi)
+{
+ pow(a * x, 2)
+}
+
+print(" ")
+print("Since the value of approximation holds, and only correct, to a decimal place- therefore, y = f(x) = x ** a does not equal π is:")
+# Comparison of values
+n = x ** a
+m = math.pi
+n < m
+{
+ (n, m):
+   print(n is not m)
+}
