03b2b5644cc061998db71742c18c4d8446e0376f
Avelanda <gorthell@gmail.com>
2021-10-15 12:00:06 +0200

    Add files via upload
    Assigning values to variables, and calling on multiple methods to compile output.

---
+++ Hello World.swift
@@ -0,0 +1,52 @@
+//Swift 5.0.1
+
+/* Copyright © 2021
+   Avelanda
+   All rights reserved
+*/
+ 
+// Concatenating strings
+   var String1 = "Hello"
+   var String2 = "World"
+   let stringPlusString = String1 + String2
+   
+// Appending strings
+   var greeting = "Hello"
+   greeting += String2
+   
+// Combination of conditional statements to create a compound expression
+   var Str3 = "Hello World"
+   let Str1 = "Hello"
+   let Str2 = "World"
+   if  Str3 == Str1 + Str2
+{
+ print("Hello World")
+// Calling a function without parameters
+} else {  
+ func sayHelloWorld() -> String 
+{
+ return "Hello World"
+ print(sayHelloWorld())
+} 
+
+ let a = "Hello"
+ let b = "World"
+ let c = a + b
+ if c == a + b
+{
+ // Combining characters for execution of a compound expression
+   var character1: Character = "H"
+   var character2: Character = "e"
+   var character3: Character = "l"
+   var character4: Character = "l"
+   var character5: Character = "o"
+   var character6: Character = "W"
+   var character7: Character = "o"
+   var character8: Character = "r"
+   var character9: Character = "l"
+   var character10: Character = "d"
+ 
+   let characterPlusCharacter = ( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ) 
+ print ("Hello World")
+}
+}
